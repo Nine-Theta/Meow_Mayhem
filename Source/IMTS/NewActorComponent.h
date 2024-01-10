74c63f9af84de086b81f3a47ad8f725b26bdedce
Nine <davidheetebrij@gmail.com>
2024-01-10 15:04:51 +0100

    missing content

--- Source/IMTS/NewActorComponent.h
+++ Source/IMTS/NewActorComponent.h
@@ -3,12 +3,12 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Components/ActorComponent.h"
+#include "Components/SceneComponent.h"
 #include "NewActorComponent.generated.h"
 
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
-class IMTS_API UNewActorComponent : public UActorComponent
+class IMTS_API UNewActorComponent : public USceneComponent
 {
 	GENERATED_BODY()
 
